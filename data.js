f775cc5ee2e6204054d81c4f926bee0e1523a15a
Ghostoperators <159180701+Ghostoperators@users.noreply.github.com>
2024-07-10 00:28:42 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,79 @@
+const data = [
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS VIKRANT',
+    title2: 'AIRCRAFT CARRIER',
+    description: 'India celebrated a historic milestone as it commissioned its first ever Indigenous Aircraft Carrier (IAC) Vikrant in September 2022, designed by Indian Navy’s in-house Warship Design Bureau (WDB) and built by Cochin Shipyard Limited. ',
+    image: 'https://www.ssbcrack.com/wp-content/uploads/2023/09/INS-Vikrant.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS GHARIAL',
+    title2: 'TANK LANDING SHIP',
+    description: 'INS Gharial is one of the tank landing ships in service with the Indian Navy. It is an amphibious warfare vessel designed to transport and deploy troops, vehicles, and equipment during amphibious operations.',
+    image: 'https://www.ssbcrack.com/wp-content/uploads/2023/09/INS-Gharial-1536x1034.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS TRIKAND',
+    title2: 'DESTROYER',
+    description: 'INS Trikand is a destroyer that has been actively contributing to the Indian Navy’s operational readiness. It is one of the Talwar-class frigates and is designed for advanced anti-submarine warfare capabilities.',
+    image: 'https://photos.fleetmon.com/vessels/ins-trikand-f51_0_632254_Large.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS MUMBAI',
+    title2: 'DESTROYER',
+    description: 'INS Mumbai is a destroyer that has been a key asset for the Indian Navy for many years. It is part of the Delhi-class destroyers and has been actively involved in numerous exercises and operations.',
+    image: 'https://1.bp.blogspot.com/_FVeEhhTjSXI/TUwuHfuRrrI/AAAAAAAABpI/fOD0zGjndj0/s1600/20090416ran8113282_045.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS KHANDERI',
+    title2: 'ATTACK SUBMARINE',
+    description: 'INS Khanderi is a conventionally powered attack submarine that plays a crucial role in the Indian Navy’s submarine fleet. ',
+    image: 'https://www.faceofnation.com/wp-content/uploads/2019/09/ezgif-2-48e851550a7c.png'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS KARANJ',
+    title2: 'ATTACK SUBMARINE',
+    description: 'INS Karanj is a conventionally powered attack submarine that strengthens the Indian Navy’s underwater warfare capabilities. ',
+    image: 'https://im.indiatimes.in/photogallery/2021/Mar/ins-karanj-navy7_60486fd1c9609.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS ARIHANT',
+    title2: 'BALLLISTIC MISSILE SUBMARINE',
+    description: 'INS Arihant is the lead ship of the Arihant-class submarines in the Indian Navy. It is India’s first indigenously built and operated nuclear-powered ballistic missile submarine. ',
+    image: 'https://www.oneindia.com/img/2016/10/20-1476946754-submarine-insarihant6.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS ARIGHAT',
+    title2: 'BALLLISTIC MISSILE SUBMARINE',
+    description: 'INS Arighat is a nuclear-powered ballistic missile submarine that serves as a critical asset for the Indian Navy. It is part of the Arihant-class submarines and has been designed to carry and launch ballistic missiles. ',
+    image: 'https://ssbcrackexams.com/wp-content/uploads/2023/03/INS-Arighat-Indias-Second-Nuclear-Submarine-To-Enter-Service-By-2024.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS SATPURA',
+    title2: 'FRIGATE',
+    description: 'INS Satpura is a frigate that has been serving the Indian Navy for several years. It is part of the Shivalik-class frigates and is designed for advanced combat capabilities.',
+    image: 'https://th-i.thgim.com/public/incoming/ojt7rw/article65934284.ece/alternates/FREE_1200/PTI06_28_2022_000192B.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS TRISHUL',
+    title2: 'FRIGATE',
+    description: 'INS Trishul is a frigate that contributes to the Indian Navy’s operational readiness. It is one of the Talwar-class frigates and is designed for advanced anti-submarine warfare capabilities. INS Trishul’s operational deployments and participation in joint exercises have reinforced the Indian Navy’s commitment to maintaining maritime security.',
+    image: 'https://i.redd.it/pzviv0g430691.jpg'
+  },
+  {
+    place: 'INDIAN NAVY',
+    title: 'INS KADMATT',
+    title2: 'FRIGATE',
+    description: 'INS Kadmatt is a frigate that enhances the Indian Navy’s anti-submarine warfare capabilities. It is part of the Kamorta-class corvettes and is equipped with advanced sonar systems and anti-submarine warfare weapons.',
+    image: 'https://english.cdn.zeenews.com/sites/default/files/2019/03/26/772602-ins-kadmatt.jpg'
+  },
+];
